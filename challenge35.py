9320895aeb0559b132df65b82135c3f4b7f295ea
Dawid Kopys <dewke17@gmail.com>
2018-06-12 19:34:36 +0200

    challenge 35 upgrade

--- challenge35.py
+++ challenge35.py
@@ -4,16 +4,19 @@ from collections import Counter
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
                                         'September', 'October', 'November', 'December']
 
-with open('ch34_birthdays.json', 'r') as f:
-    info = json.load(f)
+def get_months_count(json_file_name):
+    with open(json_file_name, 'r') as f:
+        info = json.load(f)
 
-# get months numbers 02 06 03...
-birthmonths = [info[name][3:5] for name in info.keys()]
-print(birthmonths)
+    # get months numbers 02 06 03...
+    birthmonths = [info[name][3:5] for name in info.keys()]
 
-# change to February June March...
-birthmonths = [months[int(month)-1] for month in birthmonths]
-print(birthmonths)
+    # change to February June March...
+    birthmonths = [months[int(month)-1] for month in birthmonths]
 
-c = dict(Counter(birthmonths))
-print(c)
+    c = dict(Counter(birthmonths))
+    return c
+
+if __name__=='__main__':
+    data = get_months_count('ch34_birthdays.json')
+    print(data)
