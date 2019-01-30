7fd75acb6f0e0175d52b3542aad976abaa3e292e
Dawid Kopys <dewke17@gmail.com>
2019-01-30 11:53:31 +0100

    challenge 10 solution update

--- challenge10.py
+++ challenge10.py
@@ -1,11 +1,15 @@
 import random
+from random import choices
 # a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-a = random.sample(range(1,30), 12)
-b = random.sample(range(1,30), 16)
+# a = random.sample(range(1,30), 12)
+# b = random.sample(range(1,30), 16)
+a = random.choices(range(1,30), k=12)
+b = random.choices(range(1,30), k=16)
 
 # rozw z list comprehension ale bez set - with duplicates
 common = [number for number in a if number in b]
+common.sort()
 print(common, '- list compr. w/o sets -> with duplicates')
 
 # rozw bez list comprehension
