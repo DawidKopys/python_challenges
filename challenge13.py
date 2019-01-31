26a0f03ab6e0323d65393b7535f4bbf67edfe69d
Dawid Kopys <dewke17@gmail.com>
2019-01-31 13:48:10 +0100

    challenge 13 solution update (recursive fibonaci generation)

--- challenge13.py
+++ challenge13.py
@@ -16,4 +16,17 @@ def fibonaci_generate(number):
             i = i + 1
         return fib
 
+def fibonaci_generate_rec(number):
+  if number == 0:
+    return []
+  if number == 1:
+    return [1]
+  elif number == 2:
+    return [1, 1]
+  else:
+    fib = fibonaci_generate_rec(number-1)
+    fib.append(fib[-1]+fib[-2])
+    return fib
+
 print(fibonaci_generate(u_in))
+print(fibonaci_generate_rec(u_in))
